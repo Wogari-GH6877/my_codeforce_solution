de71803ea3cc889cc393b32ca121ca2f6d27ff35
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-11-08 12:41:24 -0800

    Solution for the Problem

---
+++ A. Wrong Subtraction.py
@@ -0,0 +1,34 @@
+# num=list(map(int,input().split()))
+#
+# the_num=num[0]
+# iterate=num[1]
+# result=0
+#
+# for i in range(iterate):
+#     the_num=str(the_num)
+#
+#     if the_num[-1]=="0":
+#         result=int(the_num)//10
+#         the_num=result
+#     else:
+#
+#         result=int(the_num)-1
+#         the_num=result
+#
+#
+# print(result)
+
+# num=str(input())
+# lenght=num.count(str(4)) + num.count(str(7))
+# if lenght== len(num) :
+#     if num.count(str(4))==0 or num.count(str(7))==0:
+#         print("NO")
+#     else:
+#         print("YES")
+# else:
+#     print("NO")
+
+#
+
+
+
